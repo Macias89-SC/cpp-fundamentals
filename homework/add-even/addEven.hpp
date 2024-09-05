c78e77251de38d827999fded7cbe8d3d1c35449b
Macias89-SC <git>
2024-09-05 23:32:10 +0200

    Implement function for return sum of element from vector.

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -2,7 +2,11 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
-    // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int a =0;
+    for ( auto element : numbers) {
+        if ( element % 2 == 0 ){
+            a = a + element;
+        }
+    }
+    return a;
 }
