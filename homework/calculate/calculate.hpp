d3c68921eb54dc0be1d4da84b0abcd3f46f07ed5
Macias89-SC <git>
2024-09-05 16:43:37 +0200

    Implementation of a function solving mathematical operations.

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,33 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    if (command == "add") {
+        int AddNumber = first + second;
+        std::string str = std::to_string(AddNumber);
+        return str;
+    }
+    else if (command == "subtract"){
+        int SubNumber = first - second;
+        std:: string  str = std::to_string(SubNumber);
+        return str;
+    }
+    else if (command == "multiply"){
+        int MultiNumber = first * second;
+        std::string  str = std::to_string(MultiNumber);
+        return str;
+    }
+    else if ( command == "divide"){
+        if (second !=0){
+            int DivNumber = first / second;
+            std::string  str = std::to_string(DivNumber);
+            return str;
+        }
+        else
+        {
+            return "Division by 0";
+        }
+
+    }
+    else
+    return "Invalid data";
 }
