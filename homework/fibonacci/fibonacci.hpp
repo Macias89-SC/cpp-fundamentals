01d6cea0f3496f72e7569b0cf95e8ca961d3998c
Macias89-SC <git>
2024-09-05 17:29:50 +0200

    Implementation of calculation Fibonacci sequence

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int a,b;
+    if(sequence ==0 ) return 0;
+    a = 0;
+    b = 1;
+    for(int i=0; i < (sequence-1); i++)
+    {
+        std::swap(a,b);
+        b += a;
+    }
+    return b;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0 ){
+        return 0;
+    }
+    else if ( sequence == 1 ){
+        return 1;
+    }
+    return fibonacci_recursive(sequence-1)+ fibonacci_recursive(sequence-2);
 }
