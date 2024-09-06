9fa9ceb3d178cd29b42054d31ad78250879b6e20
Macias89-SC <git>
2024-09-06 23:42:11 +0200

    Generate Sequnce for creating Vector

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,19 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    int VectorDimension;
+    if ( count > 0 ){
+        VectorDimension = count;
+    }
+    else{
+        VectorDimension = 0;
+    }
+    std::vector<int> MyVector (VectorDimension);
+
+    if (count > 0 ){
+        for ( size_t i = 0; i < MyVector.size() ; i++  ){
+            MyVector[i] =  step * (i +1);
+        }
+    }
+    return MyVector;
 }
