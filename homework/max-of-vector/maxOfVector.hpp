8efe8ebf08ef66de1de68882d4d4de250b7ac198
Macias89-SC <git>
2024-09-07 01:40:55 +0200

    Return max value from vector

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,11 @@
 #pragma once
 #include <limits>
 #include <vector>
+#include <map>
+#include <algorithm>
+
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    return  *std::max_element(vec.begin(), vec.end());
+
 }
